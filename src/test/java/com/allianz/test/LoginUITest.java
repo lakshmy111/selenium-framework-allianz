a70b97eb89ebceb74212c46367402d011a719e7c
lakshmy111 <lakshmypriyaka@yahoo.com>
2024-04-02 00:19:58 -0700

    first commit

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,12 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+	
+	@Test(invocationCount =3)
+	public void titleTest() {
+		System.out.println("test");
+	}
+
+}
